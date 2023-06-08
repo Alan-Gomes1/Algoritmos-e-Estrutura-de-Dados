80bc5fdb08e46726fc49ffd0133c177e8a319544
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-06-08 19:50:35 -0300

    Busca binária

---
+++ Algoritmos/Binary_Search.py
@@ -0,0 +1,13 @@
+def binary_search(array, value):
+    left = 0
+    right = len(array) - 1
+
+    while left <= right:
+        middle = int((left + right) / 2)
+        if array[middle] == value:
+            return middle
+        elif array[middle] < value:
+            left = middle + 1
+        else:
+            right = middle - 1
+    return -1
